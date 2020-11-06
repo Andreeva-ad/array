9c8d887eab359349c93b675bb3a917fc424af7a2
Sarklai <andv10@mail.ru>
2020-11-06 19:10:09 +0300

    3

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace Array_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Rand = new Random();
+
+            //Массив из 10 элементов
+            int[] Array = new int[10];
+
+            //Заполняем массива элементами
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = Rand.Next(-100, 100);
+                Console.WriteLine("Array [" + i.ToString() + "] =" + Array[i]);
+            }
+        }
+    }
+}
